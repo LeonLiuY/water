e7fecc5de7806a4ee599e8ac9d09028fce6ff98b
Liu Yang <liuyang1204@hotmail.com>
2013-10-30 22:38:44 +0800

    Add markdown support.

--- water.rb
+++ water.rb
@@ -1,39 +1,58 @@
 require "sinatra"
-
+require "rdiscount"
 get "/" do
     File.read("index.html")
 end
 
 get "*" do |url|
-    html_file_path = url_to_file(url)
-    unless File.exists?(html_file_path)
-        File.read("edit.html").sub("#title", "Create me:").sub("#content", "")
+    file_path = url_to_file(url)
+    files = Dir["#{file_path}.*"]
+    if files.empty?
+        File.read("edit.html").sub("#title", "Create me:").sub("#content", "").sub("#&type", "html")
     else
+        file_path = files[0]
+        ext_name = File.extname(file_path)[1..-1]
         if params.has_key?("edit")
-            File.read("edit.html").sub("#title", "Change me:").sub("#content", File.read(html_file_path))
+            File.read("edit.html").sub("#title", "Change me:")
+            .sub("#content", File.read(file_path))
+            .sub("#&type", ext_name)
         elsif params.has_key?("delete")
             File.read("delete.html").gsub("#url", url)
         else
-            File.read(html_file_path)
+            content = File.read(file_path)
+            p ext_name
+            case ext_name
+            when 'md'
+                markdown content
+            else
+                content
+            end
         end
     end
 end
 
 post "*" do |url|
-    html_file_path = url_to_file url
-    FileUtils.mkpath(File.dirname(html_file_path))
-    File.open(html_file_path, 'w') do |file|
+    ext_name = params[:type]
+    file_path = url_to_file(url)
+    Dir["#{file_path}.*"].each do |file|
+        File.delete(file)
+    end
+    file_path = "#{file_path}.#{ext_name}"
+    FileUtils.mkpath(File.dirname(file_path))
+    File.open(file_path, 'w') do |file|
         file.write params[:text]
     end
     status 200
 end
 
 delete "*" do |url|
-    html_file_path = url_to_file url
-    File.delete(html_file_path)
+    file_path = url_to_file(url)
+    Dir["#{file_path}.*"].each do |file|
+        File.delete(file)
+    end
     status 200
 end
 
 def url_to_file url
-    "pages#{url.chomp '/'}.html"
+    "pages#{url.chomp '/'}"
 end
